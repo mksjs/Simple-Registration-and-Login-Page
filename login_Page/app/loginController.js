859e3d0a9a7ab03f234cf40531a5c3e18524f33a
Manish Kumar <mksjs1998@gmail.com>
2020-01-28 18:50:23 +0530

    loginController

--- login_Page/app/loginController.js
+++ login_Page/app/loginController.js
@@ -3,16 +3,16 @@
 // app.
 angular.module('myApp').
 controller('LoginController', function($scope, $rootScope, $stateParams, $state, LoginService) {
-$rootScope.title = "AngularJS Login Page";
-$scope.formSubmit = function() {
-if(LoginService.login($scope.username, $scope.password)) {
-$rootScope.userName = $scope.username;
-$scope.error = '';
-$scope.username = '';
-$scope.password = '';
-$state.transitionTo('home');
-} else {
-$scope.error = "Incorrect username/password !";
-}   
-};    
+    $rootScope.title = "AngularJS Login Page";
+    $scope.formSubmit = function() {
+    if(LoginService.login($scope.username, $scope.password)) {
+    $rootScope.userName = $scope.username;
+    $scope.error = '';
+    $scope.username = '';
+    $scope.password = '';
+    $state.transitionTo('home');
+    } else {
+    $scope.error = "Incorrect username/password !";
+    }   
+    };    
 });
