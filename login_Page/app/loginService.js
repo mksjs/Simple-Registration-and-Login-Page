a228f13868cb5a2f118bbddc1be00a462fb13366
Manish Kumar <mksjs1998@gmail.com>
2020-01-28 18:56:30 +0530

    loginService

--- login_Page/app/loginService.js
+++ login_Page/app/loginService.js
@@ -3,16 +3,16 @@
 // app.
 angular.module('myApp').
 factory('LoginService', function() {
-var admin = 'myanatomy';
-var pass = 'myanatomy';
-var isAuthenticated = false;
-return {
-login : function(username, password) {
-isAuthenticated = username === admin && password === pass;
-return isAuthenticated;
-},
-isAuthenticated : function() {
-return isAuthenticated;
-}
-};
+    var admin = 'myanatomy';
+    var pass = 'myanatomy';
+    var isAuthenticated = false;
+    return {
+        login : function(username, password) {
+        isAuthenticated = username === admin && password === pass;
+        return isAuthenticated;
+        },
+        isAuthenticated : function() {
+        return isAuthenticated;
+        }
+    };
 });
